bbe48c18da598d8ceb7cbb3fccbae0436b2fe0dd
fxy725 <fxy849517015@outlook.com>
2025-04-29 23:54:42 +0800

    0041

--- src/app/components/GlobalNav.tsx
+++ src/app/components/GlobalNav.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { FaHome, FaBlog, FaUser, FaTags } from "react-icons/fa";
 import ThemeToggle from "./ThemeToggle";
 
